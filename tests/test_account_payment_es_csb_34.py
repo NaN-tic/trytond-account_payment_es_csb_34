d57b0254cb28f040b5cb9765019237fd752078dc
resteve <resteve@zikzakmedia.com>
2015-11-29 11:56:59 +0100

    pyflakes

--- tests/test_account_payment_es_csb_34.py
+++ tests/test_account_payment_es_csb_34.py
@@ -2,7 +2,7 @@
 # This file is part of the account_payment_es_csb_34 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import test_view, test_depends
 import trytond.tests.test_tryton
 import unittest
 
